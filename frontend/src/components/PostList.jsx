8fe738de943150ae2de63945bf3daa760c1465b6
parth <parth@advant.xyz>
2026-03-14 23:16:45 +0530

    add insert post logic

---
+++ frontend/src/components/PostList.jsx
@@ -0,0 +1,62 @@
+import NewPost from "./NewPost";
+import Post from "./post";
+import classes from "./PostsList.module.css";
+import Modal from "./Modal";
+import { useState, useEffect } from "react";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPost] = useState([]);
+  const [isFetching, setIsFetching] = useState();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPost(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPost((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet</h2>
+          <h2>start adding some!</h2>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostsList;
